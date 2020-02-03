c0d34272f09d1ab2288265ebaf0a2a60a5eb6663
MamanFatahul <samsudinmaman140@gmail.com>
2020-02-03 08:49:02 +0700

    Tugas Besar

---
+++ resources/views/tour/Index.blade.php
@@ -0,0 +1,281 @@
+<!doctype html>
+<html>
+<head>
+<link rel="shortcut icon" href="images/002.jpg">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Mamanco TourTNTP</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+<link href="css/lightbox.css" rel="stylesheet" type="text/css">
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+<link href="css/slick.css" rel="stylesheet" type="text/css">
+<link href="css/moveTop.css" rel="stylesheet" type="text/css">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="css/preloader.css" rel="stylesheet" type="text/css">
+<link href="css/owl.carousel.css" rel="stylesheet" type="text/css">
+
+
+<script src="js/jquery.min.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div id="preloader">
+  <div id="status">&nbsp;</div>
+</div>
+
+
+	<header>
+    	<div class="col-md-12 menubar navbar-default">
+        
+        	<a href="index.html" class="navbar-brand"><img src="images/LOBO.jpg"></a>
+        	<div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" title="menu" data-target="#navbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+       		 <div id="navbar" class="navbar-collapse collapse menu">
+         	<ul class="nav navbar-nav navbar-right">
+             <li class="active"><a href="{{url('/home')}}" class="nav-link">Home</a></li>
+			<!--<li class="dropdown"><a href="" class="dropdown-toggle" data-toggle="dropdown">Home<span class="caret"></span></a>        
+                 </li>  -->
+                   
+                    <li><a href="{{url('/Tentang-kami')}}" class="nav-link">About</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    					
+						
+							
+							<li class="dropdown user-panel"><a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i></a>
+								<div class="dropdown-menu roopesh top-user-section">
+									<div class="top-user-form">
+										<form id="top-login" role="form">
+											<div class="input-group" id="top-login-username">
+												<span class="input-group-addon"><i class="fa fa-user"></i></span>
+												<input type="text" class="form-control" placeholder="Username" required>
+											</div>
+											<div class="input-group" id="top-login-password">
+												<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+												<input type="password" class="form-control" placeholder="Password" required>
+											</div>
+											<div>
+												<p class="reset-user">Forgot <a href="#">Password/Username?</a></p>
+												<button class="btn btn-danger" type="submit">Login</button>
+											</div>
+										</form>
+									</div>
+									<div class="create-account">
+										<a href="#">Create a New Account</a>
+									</div>
+								</div>
+							</li>
+						
+					
+           </ul>
+         </div>
+       	   
+         
+        
+        </div>
+        
+        
+    </header>
+    
+    <div class="container">
+                <div class="col-md-12">
+                    <div class="col-md-8">
+                        <img src="images/photo.jpg" width="100%"></<img>
+                          </div>
+
+                            <div class="col-md-4">
+                                  <h4 align = "justify"><li>Taman Nasional Tanjung Puting adalah taman nasional yang dikenal sebagai 
+                                       tempat habitat asli orang utan terbesar di dunia yang ada di indonesia. Disinilah pusat pelatihan dan pelepasan orangutan ke alam liar dilakukan. Selain orang utan ada beberapa satwa seperti bekantan, monyet, babi hutan, monyet, serta jenis satwa hutan 
+                                    lainnya yang hidup berdampingan di Taman Nasional Tanjung Puting.</li></h4>
+                                    <h4 align = "justify"><li> Taman nasional tanjung puting terletak di Kalimantan Tengah, tepatnya di Kabupaten Kotawaringin 
+                                        Barat, telah menjadi salah satu destinasi wisata Indonesia yang cukup banyak diminati oleh para wisatawan asing maupun lokal. Anda akan disajikan dengan pemandangan hutan yang asri,
+                                         menyusuri sungai amazonnya indonesia kemudian anda akan melihat habitat asli yang ada disana.</li></h4> 
+                                  </div>
+                	</div>   
+           
+            	</div>
+        
+<!-- AWAL BAGIAN KIRI -->        
+   
+    
+        <div class="col-md-12 news fit">
+         <div class="col-md-3 newstitle fit">
+        <h3><a href="#">Info Trip</a></h3>
+        <div class="col-md-12 ">
+
+            <div class="post1">
+        	<img src="images/orang.jpg" class="image1" width="100%">
+            <article class="text-center">
+            <br>
+            <h3 class="article4"><a href="#">Tanjung Puting National Park 3 Day</a></h3>
+            <div class="headinfo">
+                <i class="glyphicon glyphicon-map-marker"></i>Tanjung Puting <br/>
+                <i class="glyphicon glyphicon-time"></i>4 days 3 Nights</div>  
+            <div> 
+          <h3>  More Information <a href="#kontak" class="pop"> </h3>
+            <strong class="big"><i class="glyphicon glyphicon-phone-alt"></i> Call <a href="tel:+6282253370098" 
+                title="Klik untuk telpon langsung">+6282253370098</a></strong>
+            </a>
+            </div>
+            </article>
+        	</div>
+            <div class="post1">
+        	<img src="images/Sungai.jpg" class="image1" width="100%">
+            <article class="text-center">
+            <br>
+            <h3 class="article4"><a href="#">Tanjung Puting National Park 4 Day 3 Night</a></h3>
+            <div class="headinfo">
+                <i class="glyphicon glyphicon-map-marker"></i>Tanjung Puting <br/>
+                <i class="glyphicon glyphicon-time"></i>4 days 3 Nights 			</div> 
+
+                <h3>  More Information <a href="#kontak" class="pop"> </h3>
+                    <strong class="big"><i class="glyphicon glyphicon-phone-alt"></i> Call <a href="tel:+6282253370098" 
+                        title="Klik untuk telpon langsung">+6282253370098</a></strong>
+                    </a>
+            </article>
+        	</div>
+ <!-- AKHIR BAGIAN KIRI-->
+            
+            </div>
+            </div>
+
+ <!-- AWAL BAGIAN TENGAH -->       
+         <div class="col-md-6 newstitle">
+         <h3><a href="singlepage.html">Fasilitas Dan Keindahan</a></h3>
+            <div class="col-md-12 contbg1 wow animated fadeInUpBig" data-wow-delay="0.1s">
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/001.jpg" alt="...">
+      <div class="carousel-innertext">
+	  <h2 class="tittle"><a href="#">Kapal Dengan Fasilitas Lengkap Seperti Hotel</a></h2>
+        <h3><p>Kapal akan dilengkapi dengan kursi santai, meja makan, Spring bed seperti layaknya nginep di Hotel</p></h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/003.jpg" alt="...">
+      <div class="carousel-innertext">
+	<h2 class="tittle"><a href="#">Akan Menikmati Perjalan Melalui Hutan Belantara Sungai Sekonyer</a></h2>
+      <h3><p>Dalam perjalanan akan melihat indahnya hutan belantara Kalimantan sungai Sekonyer dan jika beruntung selama perjalanan
+            akan bertemu buaya Sungai, Bekantan, dan Hewan lainnya</p></h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/004.jpg" alt="...">
+      <div class="carousel-innertext">
+	<h2 class="tittle"><a href="#">Akan bertemu Dengan Raja Orang Utan</a></h2>
+       <h3> <p>Ketika sampai BestCamp maka kalian nanti akan melihat dengan Raja Orang Utan,
+             dan juga akan ada banyak Babi Hutan Disana </p></h3>
+      </div>
+    </div>
+   	
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+        
+     <!-- AKHIR BAGIAN TENGAH -->             
+        </div>
+        </div>
+
+    <!-- AWAL BAGIAN KANAN -->    
+        <div class="col-md-3 newstitle">
+         <h3><a href="#">Berita</a></h3>
+        <div class="col-md-12 ">
+         <div class="media media2">
+         <div class="media-left media-middle">
+        <a href="#"><img class="media-object"  src="images/006.jpg"></a>
+             </div>
+              <div class="media-body">
+              <h4><a href="#">Medunia</a></h4>
+              <p class="text-justify"><a href="#">Banyak Turis yang datang KeTanjung Puting pada setiap bulan</a></p>
+         </div>
+         </div>
+         <div class="media media2">
+         <div class="media-left media-middle">
+        <a href="#"><img class="media-object"  src="images/007.jpg"></a>
+             </div>
+              <div class="media-body">
+              <h4><a href="#">Lindungan</a></h4>
+              <p class="text-justify"><a href="#">Taman Nasional Tanjung Puting dilindungi langsung Departemen Kehutanan</a>r</p>
+         </div>
+         </div>
+         <div class="media media2">
+            <div class="media-left media-middle">
+           <a href="#"><img class="media-object"  src="images/007.jpg"></a>
+                </div>
+                 <div class="media-body">
+                 <h4><a href="#"></a></h4>
+                 <p class="text-justify"><a href="#">Taman Nasional Tanjung Puting dilindungi langsung Departemen Kehutanan</a>r</p>
+            </div>
+   
+            </div>  
+        </div>
+         	
+        
+           
+         </div>
+         </div>
+         <div class="clearfix"></div>
+         <br><br>
+         <div class="col-md-12 contbg1 socialmedia wow animated fadeInLeftBig" data-wow-delay="0.1s">
+         <ul class="social-list">
+         	<li><a href="https://web.facebook.com/?_rdc=1&_rdr"><i class="fa fa-facebook-official fa-2x"></i>Facebook</a></li>
+            <li><a href="https://twitter.com/login?lang=id"><i class="fa fa-twitter-square fa-2x"></i>Twitter</a></li>
+            <li><a href="https://www.youtube.com/watch?v=7H6ikonnBHk"><i class="fa fa-linkedin-square fa-2x"></i>Linkedin</a></li>
+            <li><a href="#"><i class="fa fa-pinterest-square fa-2x"></i>Pinterest</a></li>
+            <li><a href="#"><i class="fa fa-youtube-square fa-2x"></i>Youtube</a></li>
+         </ul>
+         </div>
+         <div class="col-md-12 contbg3 wow animated" data-wow-delay="0.1s">
+         	<div class="col-md-4">
+         </div>
+         <div class="col-xs-12 col-sm-12 col-md-2">
+            <h3> <i class="glyphicon glyphicon-map-marker" align="center"></i> ALAMAT<br/> </h3>
+             <div class="textwidget"><p>Jl. Abdul Kadir RT 11 RW 11 Kelurahan Kumai Hulu, Kecamatan Kumai, Kabupaten Kotawaringin Barat, 74181, Provinsi Kalimantan Tengah.</p>
+            </div>
+            </div>
+			<div class="col-xs-12 col-sm-12 col-md-4">
+                <h3><i class="glyphicon glyphicon-phone-alt"> KONTAK</i></h3>
+                <div class="textwidget"><p>Phone : 0822-5337-0098</p>
+                <div class="textwidget"><p>Instagram : maman_fatahul</p>
+            </div>
+         	</div>
+		
+		  </div>
+          <div class="col-md-12 col-xs-12 col-sm-12 contbg4">
+                        	<h4 class="text-center">Copyright 2019 &copy; <span class="red">MAMANCO TOUR TNTP</span> | Designed by <span class="red"></span>MAMAN_FATAHUL</span></h4>
+                        </div>
+         
+            
+ 
+
+<a href="#" class="scrollup" title="Goto Top" data-placement="top"></a>   
+<script src="js/wow.min.js" type="text/javascript"></script>
+<script>
+ new WOW().init();
+</script>  
+<script src="js/custom.js"></script>
+<script src="js/moveTop.js" type="text/javascript"></script>
+<script src="js/slick.min.js"></script> 
+<script src="js/lightbox.js"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>    
+</body>
+</html>
---
+++ resources/views/tour/Tentang.blade.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html>
+<head>
+<link rel="shortcut icon" href="images/002.jpg">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Mamanco TourTNTP</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css">
+<link href="css/lightbox.css" rel="stylesheet" type="text/css">
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+<link href="css/slick.css" rel="stylesheet" type="text/css">
+<link href="css/moveTop.css" rel="stylesheet" type="text/css">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="css/preloader.css" rel="stylesheet" type="text/css">
+<link href="css/owl.carousel.css" rel="stylesheet" type="text/css">
+
+
+<script src="js/jquery.min.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div id="preloader">
+  <div id="status">&nbsp;</div>
+</div>
+
+
+	<header>
+    	<div class="col-md-12 menubar navbar-default">
+        
+        	<a href="index.html" class="navbar-brand"><img src="images/LOBO.jpg"></a>
+        	<div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" title="menu" data-target="#navbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+       		 <div id="navbar" class="navbar-collapse collapse menu">
+         	<ul class="nav navbar-nav navbar-right">
+             <li class="active"><a href="{{url('/home')}}" class="nav-link">Home</a></li>
+			<!--<li class="dropdown"><a href="" class="dropdown-toggle" data-toggle="dropdown">Home<span class="caret"></span></a>        
+                 </li>  -->
+                   
+                    <li><a href="categorypage.html">About</a></li>
+                    <li><a href="categorypage.html">Contact Us</a></li>
+                    					
+						
+							
+							<li class="dropdown user-panel"><a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i></a>
+								<div class="dropdown-menu roopesh top-user-section">
+									<div class="top-user-form">
+										<form id="top-login" role="form">
+											<div class="input-group" id="top-login-username">
+												<span class="input-group-addon"><i class="fa fa-user"></i></span>
+												<input type="text" class="form-control" placeholder="Username" required>
+											</div>
+											<div class="input-group" id="top-login-password">
+												<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+												<input type="password" class="form-control" placeholder="Password" required>
+											</div>
+											<div>
+												<p class="reset-user">Forgot <a href="#">Password/Username?</a></p>
+												<button class="btn btn-danger" type="submit">Login</button>
+											</div>
+										</form>
+									</div>
+									<div class="create-account">
+										<a href="#">Create a New Account</a>
+									</div>
+								</div>
+							</li>
+						
+					
+           </ul>
+         </div>
+       	   
+         
+        
+        </div>
+        
+        
+    </header>
+    
+    <div class="container">
+                <div class="col-md-12">
+                    <div class="col-md-12">
+                        <img src="images/MANCOLOGO.jpg" width="100%"></<img>
+                          </div>
+
+                            <div class="col-md-12">
+                                  <h4 align = "justify"><li>MANCO Tour TNTP adalah sebuah jasa perjalanan tour untuk menuju ke Taman
+                                      Nasional Tanjung Puting dengan harga yang murah dan anda akan mendapatkan fasilitas yang tidak kalah
+                                    dengan fasilitas kapal yang lain. Jasa ini baru saja kami buka ditahun ini.</li></h4>
+                                  </div>
+                	</div>   
+           
+            	</div>
+
+        </div>
+        </div>
+
+         	
+        
+        
+         <div class="clearfix"></div>
+         <br><br>
+         <div class="col-md-12 contbg1 socialmedia wow animated fadeInLeftBig" data-wow-delay="0.1s">
+         <ul class="social-list">
+         	<li><a href="https://web.facebook.com/?_rdc=1&_rdr"><i class="fa fa-facebook-official fa-2x"></i>Facebook</a></li>
+            <li><a href="https://twitter.com/login?lang=id"><i class="fa fa-twitter-square fa-2x"></i>Twitter</a></li>
+            <li><a href="https://www.youtube.com/watch?v=7H6ikonnBHk"><i class="fa fa-linkedin-square fa-2x"></i>Linkedin</a></li>
+            <li><a href="#"><i class="fa fa-pinterest-square fa-2x"></i>Pinterest</a></li>
+            <li><a href="#"><i class="fa fa-youtube-square fa-2x"></i>Youtube</a></li>
+         </ul>
+         </div>
+         <div class="col-md-12 contbg3 wow animated" data-wow-delay="0.1s">
+         	<div class="col-md-4">
+         </div>
+         <div class="col-xs-12 col-sm-12 col-md-2">
+            <h3> <i class="glyphicon glyphicon-map-marker" align="center"></i> ALAMAT<br/> </h3>
+             <div class="textwidget"><p>Jl. Abdul Kadir RT 11 RW 11 Kelurahan Kumai Hulu, Kecamatan Kumai, Kabupaten Kotawaringin Barat, 74181, Provinsi Kalimantan Tengah.</p>
+            </div>
+            </div>
+			<div class="col-xs-12 col-sm-12 col-md-4">
+                <h3><i class="glyphicon glyphicon-phone-alt"> KONTAK</i></h3>
+                <div class="textwidget"><p>Phone : 0822-5337-0098</p>
+                <div class="textwidget"><p>Instagram : maman_fatahul</p>
+            </div>
+         	</div>
+		
+		  </div>
+          <div class="col-md-12 col-xs-12 col-sm-12 contbg4">
+                        	<h4 class="text-center">Copyright 2019 &copy; <span class="red">MAMANCO TOUR TNTP</span> | Designed by <span class="red"></span>MAMAN_FATAHUL</span></h4>
+                        </div>
+         
+            
+ 
+
+<a href="#" class="scrollup" title="Goto Top" data-placement="top"></a>   
+<script src="js/wow.min.js" type="text/javascript"></script>
+<script>
+ new WOW().init();
+</script>  
+<script src="js/custom.js"></script>
+<script src="js/moveTop.js" type="text/javascript"></script>
+<script src="js/slick.min.js"></script> 
+<script src="js/lightbox.js"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>    
+</body>
+</html>
